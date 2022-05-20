ac0c69dd90f1b867dd6721b3a602c994d35a8617
VTV1414 <rolando11121@gmail.com>
2022-05-20 14:28:56 +0300

    main

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,5 +1,8 @@
 const getData = () => {
-    return fetch('https://test-f327a-default-rtdb.firebaseio.com/goods.json')
+    return fetch(
+        `https://test-f327a-default-rtdb.firebaseio.com/goods.json`
+        //`https://test-f327a-default-rtdb.firebaseio.com/goods.json?${str ? `search=${str}` : ''}`
+    )
         .then((response) => {
             return response.json()
         })
